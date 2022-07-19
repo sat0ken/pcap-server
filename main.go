c40878837c2c4db315e1355f57b837dc98c16d08
sat0ken <15720506+sat0ken@users.noreply.github.com>
2022-07-19 09:50:58 +0900

    update

--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"fmt"
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	"log"
 )
 
 func main() {
+	var clientPaket, serverPacket [][]byte
+
 	handle, err := pcap.OpenOffline("./local-http3.pcapng")
 	if err != nil {
 		log.Fatal(err)
 	}
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		fmt.Printf("packet is %+v\n", packet.TransportLayer())
+		//fmt.Printf("packet is %x\n", packet.ApplicationLayer().Payload())
+		udpLayer := packet.Layer(layers.LayerTypeUDP)
+		udp, _ := udpLayer.(*layers.UDP)
+		if udp.DstPort == 18443 {
+			serverPacket = append(serverPacket, udp.Payload)
+		} else {
+			clientPaket = append(clientPaket, udp.Payload)
+		}
 	}
+	fmt.Printf("server packet is %x\n", serverPacket)
 }
